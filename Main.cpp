30b8f15d874aec374526039f3879672a767d01c2
Joaqo Fernandez <jqcfernandez@gmail.com>
2025-10-07 09:48:50 -0500

    A few more polishing updates

--- Main.cpp
+++ Main.cpp
@@ -5,45 +5,48 @@
 #include "Player.h"
 using namespace std;
 
+struct RoundResult {
+  Player* winner;
+  Player* loser;
+};
+
+int verifyBets(Player& bettor, Player& other){
+  int raise = 0;
+  do{
+    cout << bettor.name + ", you must raise until you match or surpass " + other.name + "'s bet or enter 0 to fold. Your current bet is " + to_string(bettor.bet) + ". How much do you want to raise your bet by? ";
+    cin >> raise;
+    while(bettor.bet + raise > bettor.deck.size() || bettor.bet + raise > other.deck.size()){
+      cout << "Your bet has exceeded a player's deck size. Please enter a lower bet: ";
+      cin >> raise;
+    }
+    while(raise < 0){
+      cout << "Your bet cannot be negative. Try again: ";
+      cin >> raise;
+    }
+  } while(bettor.bet + raise < other.bet && raise != 0);
+  return raise;
+}
+
 int getBets(Player& lowerSuit, Player& higherSuit){
   int raise;
   cout << lowerSuit.name + ", enter your bet: ";
   cin >> raise;
   while(raise <= 0 || raise > lowerSuit.deck.size() || raise > higherSuit.deck.size()){
-    cout << "Your bet must be greater than 0 and less than or equal to the number of cards either player has left. Enter a new bet: ";
+    cout << "Your bet must be greater than 0 and cannot exceed a player's deck. Enter a new bet: ";
     cin >> raise;
   }
   lowerSuit.bet += raise;
   while(higherSuit.bet != lowerSuit.bet){
-    while(higherSuit.bet < lowerSuit.bet && raise != 0){
-      cout << higherSuit.name + ", you must raise until you match or surpass " + lowerSuit.name + "'s bet or enter 0 to fold. Your current bet is " + to_string(higherSuit.bet) + ". How much do you want to raise your bet by? ";
-      cin >> raise;
-      while(higherSuit.bet + raise > higherSuit.deck.size() || higherSuit.bet + raise > lowerSuit.deck.size()){
-        cout << "Your bet has exceeded a player's deck size. Please enter a lower bet: ";
-        cin >> raise;
-      }
-      while(raise < 0){
-        cout << "Your bet cannot be negative. Try again: ";
-        cin >> raise;
-      }
-      higherSuit.bet += raise;
-    }
+    raise = verifyBets(higherSuit, lowerSuit);
+    higherSuit.bet += raise;
     if(raise == 0){
       return -1;
     }
-    while(lowerSuit.bet < higherSuit.bet && raise != 0){
-      cout << lowerSuit.name + ", you must raise until you match or surpass " + higherSuit.name + "'s bet or enter 0 to fold. Your current bet is " + to_string(lowerSuit.bet) + ". How much do you want to raise your bet by? ";
-      cin >> raise;
-      while(lowerSuit.bet + raise > higherSuit.deck.size() || lowerSuit.bet + raise > lowerSuit.deck.size()){
-        cout << "Your bet has exceeded a player's deck size. Please enter a lower bet: ";
-        cin >> raise;
-      }
-      while(raise < 0){
-        cout << "Your bet cannot be negative. Try again: ";
-        cin >> raise;
-      }
-      lowerSuit.bet += raise;
+    if(higherSuit.bet == lowerSuit.bet){
+      break;
     }
+    raise = verifyBets(lowerSuit, higherSuit);
+    lowerSuit.bet += raise;
     if(raise == 0){
       return -1;
     }
@@ -51,7 +54,8 @@ int getBets(Player& lowerSuit, Player& higherSuit){
   return higherSuit.bet;
 }
 
-Player* compareCards(Player& p1, Player& p2){
+
+RoundResult compareCards(Player& p1, Player& p2, bool war){
   cin.get();
   Card p1Card = p1.deck.front();
   cout << p1.name + " plays the " + p1Card.print() + "\n";
@@ -59,18 +63,27 @@ Player* compareCards(Player& p1, Player& p2){
   Card p2Card = p2.deck.front();
   cout << p2.name + " plays the " + p2Card.print() + "\n";
   p2.deck.pop();
-  Player* winner;
+  RoundResult result;
   if(p1Card.number > p2Card.number){
     cout << p1.name + " wins!\n";
     p1.deck.push(p1Card);
     p1.deck.push(p2Card);
-    winner = &p1;
+    result.winner = &p1;
+    result.loser = &p2;
   }
   else if(p2Card.number > p1Card.number){
     cout << p2.name + " wins!\n";
     p2.deck.push(p2Card);
     p2.deck.push(p1Card);
-    winner = &p2;
+    result.winner = &p2;
+    result.loser = &p1;
+  }
+  else if(p1.deck.size() == 0 || p2.deck.size() == 0){//the very niche situation where a player's last card is a war. In this case, they immediately lose
+    result.winner = (p1.deck.empty()) ? &p2 : &p1;
+    result.loser = (p2.deck.empty()) ? &p2 : &p1;
+    cout << result.loser->name + " doesn't have enough cards to play the war. Therefore they automatically lose the game.\n";
+    result.winner->deck.push(p1Card);
+    result.winner->deck.push(p2Card);
   }
   else{
     cout << "We have a war!\n";
@@ -81,53 +94,31 @@ Player* compareCards(Player& p1, Player& p2){
     else{
       bet = getBets(p1, p2);
     }
-    if(bet < 0){
-      if(p1.bet < p2.bet){
-        winner = &p2;
-        for(int i = 0; i < p1.bet; i++){
-          p2.deck.push(p1.deck.front());
-          p1.deck.pop();
-        }
-      }
-      else{
-        winner = &p1;
-        for(int i = 0; i < p2.bet; i++){
-          p1.deck.push(p2.deck.front());
-          p2.deck.pop();
-        }
-      }
+    if(bet < 0 && p1.bet < p2.bet){
+      result.winner = &p2;
+      result.loser = &p1;
+    }
+    else if(bet < 0){
+      result.winner = &p1;
+      result.loser = &p2;
+      //cin.get();
     }
     else{
-      winner = compareCards(p1, p2);
-      if(winner == &p1){
-        for(int i = 0; i < p2.bet; i++){
-          p1.deck.push(p2.deck.front());
-          p2.deck.pop();
-        }
-      }
-      else{
-        for(int i = 0; i < p1.bet; i++){
-          p2.deck.push(p1.deck.front());
-          p1.deck.pop();
-        }
-      }
+      result = compareCards(p1, p2, true);
+      //cin.get();
+    }
+    for(int i = 0; i < result.loser->bet; i++){
+      result.winner->deck.push(result.loser->deck.front());
+      result.loser->deck.pop();
     }
     p1.bet = 0;
     p2.bet = 0;
-    //Pseudocode for placing bets:
-    //The player with the worse suit bets first
-    //Second player must match, raise or fold
-    //This continues in loop until someone matches or folds
-    //Each player:
-    //Pop top card (card for war)
-    //Pop bet and add to array
-    //Compare war cards
-    //Winner takes pot 
-    winner->deck.push(p1Card);
-    winner->deck.push(p2Card);//These are the war cards so their values are the same
+    result.winner->deck.push(p1Card);
+    result.winner->deck.push(p2Card);//These are the war cards so their values are the same, therefore adding order doesn't matter
   }
-  cout << to_string(p1.deck.size()) + ", " + to_string(p2.deck.size()) + "\n";
-  return winner;
+  if(!war){cout << p1.name << "'s deck size: " << p1.deck.size() << "\n";
+    cout << p2.name << "'s deck size: " << p2.deck.size()<< "\n";}
+  return result;
 }
 
 int main() {
@@ -143,7 +134,7 @@ int main() {
 
   //The actual gameplay code
   while(!p1.deck.empty() && !p2.deck.empty()){
-    compareCards(p1, p2);
+    compareCards(p1, p2, false);
   }
   if(p1.deck.empty()){
     cout << p2.name + " wins the game!";
